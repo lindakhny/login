c1ddbb1e96372722e47cee915bc9d408d9aaa41b
lindakhny <kh.inayah@gmail.com>
2017-11-25 06:48:08 +0700

    Just testing

---
+++ app/src/main/java/com/android/iak3/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.android.iak3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText etUname;
+    private EditText etPass;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUname = (EditText)findViewById(R.id.et_uname);
+        etPass = (EditText)findViewById(R.id.et_pass);
+
+        btnLogin = (Button)findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUname.getText().toString();
+                String password = etPass.getText().toString();
+
+                if(username.equals("android")&&(password.equals("kejar"))){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Username atau Password salah", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
